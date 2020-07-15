090904d5dbcef034d9285db993d161724e054c31
Jiwon Lee <58061467+Jinnie-Lee@users.noreply.github.com>
2020-07-15 16:38:57 +0900

    Create 타일 채우기.py

---
+++ 타일 채우기.py
@@ -0,0 +1,32 @@
+#2X1, 1X2, 2X2 타일로 2Xn 짜리 직사각형 채우기
+
+#입력 직사각형 가로 길이 n(1~100, 정수), 입력값 m(1~400, 정수)
+#출력 경우의수를 m으로 나눈 나머지
+
+n, m = map(int, input().split(" "))
+
+def tile(n):
+	if n==1:
+		return 1
+	elif n==2:
+		return 3
+	else:
+		return tile(n-1)+tile(n-2)*2
+	
+print(tile(n)%m)
+
+
+cache = [-1 for i in range(1001)]
+def tile2(n):
+	if n==1:
+		return 1
+	elif n==2:
+		return 3
+	
+	if cache[n]!=-1:
+		return cache[n]
+	
+	cache[n]=tile2(n-1)+tile2(n-2)*2
+	return cache[n]
+
+print(tile2(n)%m)
