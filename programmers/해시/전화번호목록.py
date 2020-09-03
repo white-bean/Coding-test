3984d619ca8818d4bee2c4bdcf136c2abfb7e28f
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2020-09-03 15:35:29 +0900

    Update 전화번호목록.py

--- programmers/해시/전화번호목록.py
+++ programmers/해시/전화번호목록.py
@@ -14,3 +14,53 @@ def solution(phone_book):
             if phone_book[i] == phone_book[j][0:len(phone_book[i])]:
                 return False
     return True
+
+
+
+
+# 다른 사람 풀이 1
+
+def solution(phone_book):
+    answer = True 
+    phone_book.sort() 
+    for i in range(len(phone_book)-1): 
+        if phone_book[i] in phone_book[i+1]:    # 'in' 명령어 기억하자! 꽤 효율적인듯
+            answer = False 
+            break 
+            
+    return answer
+
+
+# 다른 사람 풀이 2
+def solution(phone_book):
+    phone_book.sort()
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+# zip함수로 바로 옆 원소 묶음
+# p2.startswith(p1) : 문자열 p1이 문자열 p2 맨 앞에 있다면 true, 아니면 false
+
+
+# 다른 사람 풀이 3
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+        
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
+
+# hash_map은 {'119': 1, '97674223': 1, ' 1195524421': 1}이 된다.
+# 첫 번째 반복의 phone_number= "119"이고 number="1", temp="1"이고, temp자체가 hash_map에 없으므로 다음 반복
+# number="1", temp="11"이 되는데 11도 hash_map에 없음
+# number="9", temp="119가 되는데 119는 hash_map에 있음, 그러나 temp="119"와 phone_number ="119"와 같으므로 다음 반복
+# 쭈우욱 반복해서 phone_number = "1195524421"일 때 temp = "119"까지 반복하면 조건이 만족해서 answer=False로 뜰 것이다.
+# 출처 : https://assaeunji.github.io/python/2020-05-08-pgphone/
