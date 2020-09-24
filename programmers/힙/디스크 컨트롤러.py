18014f7a34eb4ef3ac3410563d943e58af9a8d2f
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2020-09-25 02:28:54 +0900

    Create 디스크 컨트롤러.py

---
+++ programmers/힙/디스크 컨트롤러.py
@@ -0,0 +1,28 @@
+# jobs의 길이는 1 이상 500 이하입니다.
+# jobs의 각 행은 하나의 작업에 대한 [작업이 요청되는 시점, 작업의 소요시간] 입니다.
+# 각 작업에 대해 작업이 요청되는 시간은 0 이상 1,000 이하입니다.
+# 각 작업에 대해 작업의 소요시간은 1 이상 1,000 이하입니다.
+# 하드디스크가 작업을 수행하고 있지 않을 때에는 먼저 요청이 들어온 작업부터 처리합니다.
+
+# 각 작업에 대해 [작업이 요청되는 시점, 작업의 소요시간]을 담은 2차원 배열 jobs가 매개변수로 주어질 때, 
+# 작업의 요청부터 종료까지 걸린 시간의 평균을 가장 줄이는 방법으로 처리하면 평균이 얼마가 되는지 return 하도록 solution 함수를 작성해주세요. (단, 소수점 이하의 수는 버립니다)
+
+def solution(jobs):
+    answer = 0
+    start = 0  # 현재까지 진행된 작업 시간
+    length = len(jobs)
+
+    jobs = sorted(jobs, key=lambda x: x[1])  # 소요시간 기준으로 오름차순 정렬
+
+    while len(jobs) != 0:
+        for i in range(len(jobs)):
+            if jobs[i][0] <= start:
+                start += jobs[i][1]
+                answer += start - jobs[i][0]
+                jobs.pop(i)
+                break
+            # 해당시점에 아직 작업이 들어오지 않았으면 시간 +1
+            if i == len(jobs) - 1:
+                start += 1
+
+    return answer // length
