2f410d318ac1f5c51610005b507450f321bf9615
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-02-07 22:58:28 +0900

    [Silver IV] Title: 30, Time: 64 ms, Memory: 32988 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -1,11 +1,9 @@
-from itertools import permutations
-N = int(input())
-tmp = sorted(list(map(int, str(N))), reverse=True)
-#print(tmp)
-if 0 not in tmp:
+N = input()
+tmp = sorted(N, reverse=True)
+if '0' not in tmp:
     print(-1)
 else:
-    if sum(tmp) % 3 == 0:
-        print("".join(list(map(str, tmp))))
+    if sum(list(map(int, tmp))) % 3 == 0:
+        print("".join(tmp))
     else:
         print(-1)
