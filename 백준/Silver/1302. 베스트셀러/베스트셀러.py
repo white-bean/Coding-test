4b47808fb4902684bdc1809775910ebe12f6b888
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-02-07 22:23:45 +0900

    [Silver IV] Title: 베스트셀러, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,10 +1,3 @@
 N = int(input())
 books = sorted([input() for _ in range(N)])
-books_dict = {}
-for book in books:
-    if book not in books_dict:
-        books_dict[book] = 1
-    else:
-        books_dict[book] += 1
-
-print([k for k,v in books_dict.items() if max(books_dict.values()) == v][0])
+print(max(books, key=books.count))
