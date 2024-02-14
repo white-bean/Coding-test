67424b1c8fdff5006e92aeb8f156a21c066e9a26
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-02-14 22:30:49 +0900

    [Silver I] Title: 안전 영역, Time: 912 ms, Memory: 34112 KB -BaekjoonHub

---
+++ 백준/Silver I/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N = int(input())
+area = [list(map(int, input().strip().split())) for _ in range(N)]
+
+def bfs(graph, i, j, h, visited):
+    q = deque()
+    q.append((i, j))
+    visited[i][j] = 1
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    while q:
+        a, b = q.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if nx <0 or nx>=N or ny <0 or ny>=N: continue
+            if (graph[nx][ny] > h) & (visited[nx][ny]==0):
+                visited[nx][ny] = 1
+                q.append((nx, ny))
+
+result = []
+for h in range(0, max(map(max, area))+1):
+    cnt = 0
+    visited = [[0]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if (area[i][j] > h) & (visited[i][j]==0):
+                bfs(area, i, j, h, visited)
+                cnt += 1
+    result.append(cnt)
+
+print(max(result))
