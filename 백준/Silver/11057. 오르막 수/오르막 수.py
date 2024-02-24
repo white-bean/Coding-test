c508fac46ebe0854d9dccc454dbcb66d4db84514
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-02-24 23:09:46 +0900

    [Silver I] Title: 오르막 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -1,9 +1,6 @@
 N = int(input())
-DP = [[1]*10 for _ in range(N+1)]
-if N == 1:
-    print(sum(DP[1]))
-else:
-    for i in range(2, N+1):
-        for j in range(10):
-            DP[i][j] = sum(DP[i-1][j:10])
-    print(sum(DP[N])%10007)
+DP = [1]*10
+for i in range(1, N):
+    for j in range(1, 10):
+        DP[j] += DP[j-1]
+print(sum(DP)%10007)
