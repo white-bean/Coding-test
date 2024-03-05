c224658c355a61db41865aa110cd667dd7475b82
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-03-05 22:07:15 +0900

    [Silver I] Title: 신입 사원, Time: 3664 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,15 +1,12 @@
 import sys
 input = sys.stdin.readline
-#1946 신입사원
 T = int(input())
 for _ in range(T):
     N = int(input())
     app = []
     for _ in range(N):
-        dcm, itv = map(int, input().split())
-        app.append([dcm, itv])
-    app.sort(key=lambda x: (x[0], x[1]))
-    #print(app)
+        app.append(list(map(int, input().split())))
+    app.sort(key=lambda x: x[0])
     cnt = 1;min = app[0][1]
     for i in range(1, N):
         if min > app[i][1]:
