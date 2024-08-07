27ca9e61c7de0f4b98234c2daf398ca4f971d718
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-08-07 16:07:04 +0900

    [Silver IV] Title: 국영수, Time: 372 ms, Memory: 63164 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -3,8 +3,8 @@ input = sys.stdin.readline
 N = int(input())
 alist = []
 for _ in range(N):
-    a, b, c, d = map(str, input().split())
-    alist.append([a, int(b), int(c), int(d)])
+    tmp = input().split()
+    alist.append([tmp[0], int(tmp[1]), int(tmp[2]), int(tmp[3])])
 
 alist.sort(key = lambda x: (-x[1], x[2], -x[3], x[0]))
 
