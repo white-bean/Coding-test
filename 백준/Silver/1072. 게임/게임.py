b13f27888163b2cf2e2d413b10e7a755a2102aba
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-08-11 20:04:28 +0900

    [Silver III] Title: 게임, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,12 +1,12 @@
-import sys, math
+import sys
 input = sys.stdin.readline
 
 X, Y = map(int, input().split())
 start, end = 1, 10**9
-Z = math.trunc((Y*100)/X)
+Z = (Y*100)//X
 while start <= end:
     mid = (start + end) // 2
-    new_Z = math.trunc(((Y+mid)*100)/(X+mid))
+    new_Z = ((Y+mid)*100)//(X+mid)
     if new_Z > Z: 
         end = mid - 1
     else: start = mid + 1
