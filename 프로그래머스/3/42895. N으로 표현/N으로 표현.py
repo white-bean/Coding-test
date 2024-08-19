09bf2c2acf00f2201210c43581f2a478cbac6a2a
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-08-19 22:03:36 +0900

    [level 3] Title: N으로 표현, Time: 38.50 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,17 +1,26 @@
+def calculating(X, Y):
+    result = set()
+    for x in X:
+        for y in Y:
+            result.add(x+y)
+            result.add(x-y)
+            result.add(y-x)
+            result.add(x*y)
+            if y != 0:
+                result.add(x//y)
+    return result
+
 def solution(N, number):
-    answer = 0
-    if N==number: return 1
-    DP = [{int(str(N)*i)} for i in range(1, 9)]
-    for i in range(1, 8):
-        for j in range(i//2+1):
-            for num in list(DP[j]):
-                for num2 in list(DP[i-j-1]):
-                    DP[i].add(num+num2)
-                    DP[i].add(num-num2)
-                    DP[i].add(num2-num)
-                    DP[i].add(num*num2) 
-                    if num != 0: DP[i].add(num2//num)
-                    if num2 != 0: DP[i].add(num//num2)
-        if number in list(DP[i]):
-            return i+1
-    return -1
+    answer = -1
+    if N == number: return 1
+    dp = {}
+    dp[1] = {N}
+    for i in range(2, 9):
+        temp = {int(str(N)*i)}
+        for j in range(1, i):
+            temp.update(calculating(dp[j], dp[i-j]))
+        if number in temp:
+            answer = i
+            break
+        dp[i] = temp
+    return answer
