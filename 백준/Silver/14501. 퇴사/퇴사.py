123ff043ef48f0bb8a21ce5e72132afae4d3fbd3
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-08-19 17:11:54 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -3,14 +3,13 @@ input = sys.stdin.readline
 
 N = int(input())
 T, P, dp = [0] * (N+2), [0] * (N+2), [0] * (N+2)
-result = 0 # 금액의 최대값
 for i in range(1, N+1):
     a, b = map(int, input().split())
     T[i], P[i] = a, b
 
-for i in range(1, N+2):
-    dp[i] = max(dp[i], result)
+for i in range(1, N+1):
     if i+T[i] <= N+1:
         dp[i+T[i]] = max(dp[i+T[i]], dp[i] + P[i])
-    result = max(dp[i], result)
-print(result)
+    dp[i+1] = max(dp[i], dp[i+1])
+    
+print(dp[N+1])
