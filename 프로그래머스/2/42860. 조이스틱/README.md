3090afb62036693bde8335a85322cfb2522f7c5b
Jiwon Lee <58061467+white-bean@users.noreply.github.com>
2024-09-24 15:01:15 +0900

    [level 2] Title: 조이스틱, Time: 236.19 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.04 ms
+메모리: 13 MB, 시간: 236.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 24일 14:01:27
+2024년 09월 24일 15:01:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,12 +1,23 @@
+from collections import deque
+import copy
+
 def solution(name):
     answer = 0
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] # N = 13
     if name == 'A'*len(name): return 0
-    leftright = len(name)-1 # 좌우로 이동할 수 있는 최대값
-    for i, s in enumerate(name):
-        answer += min(alphabet.index(s), 26 - alphabet.index(s)) # 상하
-        notA = i + 1 # A가 아닌 알파벳 위치
-        while (notA < len(name)) and (name[notA] == 'A'): notA += 1
-        leftright = min(leftright, i + len(name) - notA + min(i, len(name) - notA))
-    answer += leftright
-    return answer
+    name = list(name)
+    dx = [-1, 1]
+    def bfs(alist, cnt, idx):
+        queue = deque([(alist, cnt, idx)])
+        while queue:
+            alist, cnt, idx = queue.popleft()
+            cnt += min(alphabet.index(alist[idx]), 26 - alphabet.index(alist[idx]))
+            alist[idx] = 'A'
+            if alist == ['A']*len(alist):
+                return cnt
+            for i in dx:
+                new_idx = (idx + i) % len(alist)
+                new_alist = copy.deepcopy(alist)
+                queue.append((new_alist, cnt + 1, new_idx))
+            
+    return bfs(name, 0, 0)
